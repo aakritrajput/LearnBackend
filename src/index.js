af3dcc739d06f0a22e78eb48801068ad13ec3b7b
aakritrajput <aakritrajput87@gmail.com>
2024-12-24 11:41:46 +0530

    Utill code discussion

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 //require('dotenv').config({path:'./env'}) // in consig give an object that contains path of the env 
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
     path: './env'
 })
 
 connectDB()
+ .then(()=>{
+    app.on("error", (error)=>{     // listener after database connects for any error 
+        console.log("ERROR:", error)
+        throw error
+    })
+    app.listen(process.env.PORT || 8000 ,()=>{
+        console.log(`Server is running at port : ${process.env.PORT}`)
+    })
+ })
+ .catch((error)=>{
+    console.log("MONGO db connection failed!!! ", error)
+ })
 
 
 
@@ -37,7 +50,7 @@ connectDB()
 ( async()=>{
     try {
         await mongoose.connect(`${process.env.MMONGODB_URI}/${DB_NAME}`)
-        app.on()("error", (error)=>{     // listener after database connects 
+        app.on("error", (error)=>{     // listener after database connects 
             console.log("ERROR:", error)
             throw error
         })
